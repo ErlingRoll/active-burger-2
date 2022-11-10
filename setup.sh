f79e72b8736fe75d819ad43cdca6b3ab0668d218
Erling Roll <erlingroll98@gmail.com>
2022-11-10 16:06:14 +0100

    [backend]: Fix setup

--- setup.sh
+++ setup.sh
@@ -8,3 +8,6 @@ docker image pull postgres:15
 docker run --name $PG_CONTAINER_NAME -e POSTGRES_PASSWORD=$PG_PASSWORD -e POSTGRES_USER=$PG_USER -e POSTGRES_DB=$PG_DB -p 5432:5432 -d postgres:15
 # Run PostgreSQL init script
 cat ./pgsql_init.sql | docker exec -i $PG_CONTAINER_NAME psql -U $PG_USER -d $PG_DB
+
+# Install nodemon for hot reload
+npm install -g nodemon
